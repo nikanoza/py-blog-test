7d65dc00beeb47f89391870f07e23b43601bb70c
Nika Nozadze <nikanozadze1010@gmail.com>
2023-09-19 09:12:16 +0400

    refactor: change image fields in models

--- blog_app/models.py
+++ blog_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
-    avatar = models.ImageField(upload_to='authors/', null=True, blank=True)
+    avatar = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -12,7 +12,20 @@ class Author(models.Model):
 class Category(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    logo = models.ImageField(upload_to='category_logos/', null=True, blank=True)
+     logo = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.title
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=300)
+    text = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    likes = models.PositiveIntegerField(default=0)
+    views = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+    
